b414ca1eb9ef6ddca97ea4e53de853f75a95500a
Joshua Lin <joshualin555@gmail.com>
2024-09-09 12:56:21 +0800

    Add documentation

--- q3.py
+++ q3.py
@@ -2,13 +2,16 @@
 This function takes an integer n and returns the nth term of the Fibonacci sequence.
 '''
 
-mem = {0: 0, 1: 1} # Keeps track of values that have already been calculated (memoization).
+# Used to keep track of values that have already been calculated (memoization).
+mem = {0: 0, 1: 1}
 
 def fib(n):
     # Base case.
+    # Also used for memoization by checking if value has already been calculated.
     if n in mem:
         return mem[n]
-    
+
+    # Recursively calculate fibonacci sequence.
     mem[n] = fib(n - 1) + fib(n - 2)
     return mem[n]
 
